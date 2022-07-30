7d2695cda8dc2416aab440604090f03be1cc7dcf
Estanislao Alfonso <estanislaoalfonso@MacBook-Air-de-Estanislao.local>
2022-07-30 11:06:32 -0300

    primer commit de la web-app

---
+++ src/components/main.jsx
@@ -0,0 +1,20 @@
+import '../style/main.css'
+
+const Main = () => {
+    return (
+        <div className='mainContainer'>
+            <img src="https://i.imgur.com/rG2gM0G.jpg" alt="foto principal" />
+            <ul>
+                <li>
+                    <a href="https://github.com/estanislaoalfonso">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/estanislao-alfonso-a6686b107/">LinkedIn</a>
+                </li>
+                <li>estanislao.alf@gmail.com</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main;
