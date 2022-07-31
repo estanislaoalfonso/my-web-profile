d3ded62632d206ac6dbff69b5c89bcd24ff6efcb
Estanislao Alfonso <estanislaoalfonso@MacBook-Air-de-Estanislao.local>
2022-07-31 10:53:34 -0300

    adding icons

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,10 @@
 import '../style/main.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  {faEnvelope}  from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+
+
 
 const Main = () => {
     return (
@@ -6,12 +12,17 @@ const Main = () => {
             <img src="https://i.imgur.com/rG2gM0G.jpg" alt="foto principal" />
             <ul>
                 <li>
+                    <FontAwesomeIcon icon= {faGithub}/>
                     <a href="https://github.com/estanislaoalfonso">GitHub</a>
                 </li>
                 <li>
+                    <FontAwesomeIcon icon = {faLinkedin}/>
                     <a href="https://www.linkedin.com/in/estanislao-alfonso-a6686b107/">LinkedIn</a>
                 </li>
-                <li>estanislao.alf@gmail.com</li>
+                <li>
+                <FontAwesomeIcon icon={faEnvelope} />
+                <a href="https://www.linkedin.com/in/estanislao-alfonso-a6686b107/">estanislao.alf@gmail.com</a>
+                </li>
             </ul>
         </div>
     )
