9ebb89a21a0c2f1ebecb3292851e57c95197a591
Estanislao Alfonso <estanislaoalfonso@MacBook-Air-de-Estanislao.local>
2022-08-02 22:07:13 -0300

    refreshing from

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,18 +1,24 @@
-import "../style/form.css"
+import "../style/form.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import {Link} from 'react-router-dom';
 
 const Form = () => {
     return (
-        <form action="" className="formContainer">
-            <h2>What's your name ?</h2>
-            <input type="text" className="formFromBox"/>  
-            
-            <h3>What can I help you with ? </h3>
-            <input type="text" className="formSubjectBox"/>
-
-            <input type="text" className="formMessageBox"/>
-
+        <div className="formGeneral">
+        <form className="formContainer">
+            <h2>Leave me a message !</h2>
+            <input type="text" className="formFromBox" placeholder="Your name"/>  
+            <input type="text" className="formFromBox" placeholder="Your e-mail"/>
+            <input type="text" className="formFromBox" placeholder="Subject"/>
+            <textarea type="text" className="formMessageBox"/>
             <button type="submit" className="formButton">SEND</button>
+            
+            <Link to='/'>
+                <FontAwesomeIcon icon={faHouse} className='houseIcon'/>
+            </Link>
         </form>
+        </div>
     )
 }
 
