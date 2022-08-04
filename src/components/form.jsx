626aa5071b3b9742ca637e9cb0f66c3224611548
Estanislao Alfonso <estanislaoalfonso@MacBook-Air-de-Estanislao.local>
2022-08-04 08:43:06 -0300

    adding emailjs service

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,16 +2,35 @@ import "../style/form.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import {Link} from 'react-router-dom';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm("service_ra50zye", "contact_form", form.current, "Sp7pvEG-Z1c5mUt8z")
+        .then (
+            result => {
+                alert ('Correo enviado correctamente');
+            },
+        error => {
+            alert (`Error found, try again - ${error}`)
+        }
+        )
+    }
+
     return (
         <div className="formGeneral">
-        <form className="formContainer">
+        <form ref={form} className="formContainer" onSubmit={sendEmail}>
             <h2>Leave me a message !</h2>
-            <input type="text" className="formFromBox" placeholder="Your name"/>  
-            <input type="text" className="formFromBox" placeholder="Your e-mail"/>
-            <input type="text" className="formFromBox" placeholder="Subject"/>
-            <textarea type="text" className="formMessageBox"/>
+            <input type="text" className="formFromBox" placeholder="Your name" name="from_name"/>  
+            <input type="text" className="formFromBox" placeholder="Your e-mail" name="user_email"/>
+            {/* <input type="text" className="formFromBox" placeholder="Subject"/> */}
+            <textarea type="text" className="formMessageBox" name="message"/>
             <button type="submit" className="formButton">SEND</button>
             
             <Link to='/'>
