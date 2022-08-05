fd4344539fc3ea14196ae3a526259db037860368
Estanislao Alfonso <estanislaoalfonso@MacBook-Air-de-Estanislao.local>
2022-08-05 08:32:09 -0300

    form adjustments

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -8,6 +8,9 @@ import emailjs from 'emailjs-com';
 const Form = () => {
 
     const form = useRef();
+    const nameResetValues = useRef();
+    const emailResetValues = useRef();
+    const textAreaResetValues = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -15,22 +18,25 @@ const Form = () => {
         emailjs.sendForm("service_ra50zye", "contact_form", form.current, "Sp7pvEG-Z1c5mUt8z")
         .then (
             result => {
-                alert ('Correo enviado correctamente');
+                alert ('Thank you! =). Your e-mail has been sent. I will give you my feedback as soon as possible !.');
             },
         error => {
-            alert (`Error found, try again - ${error}`)
+            alert (`Something went wrong, please try to contact me again.`)
         }
         )
+        .then (nameResetValues.current.value = "",
+        emailResetValues.current.value = "",
+        textAreaResetValues.current.value = "")
+
     }
 
     return (
         <div className="formGeneral">
         <form ref={form} className="formContainer" onSubmit={sendEmail}>
             <h2>Leave me a message !</h2>
-            <input type="text" className="formFromBox" placeholder="Your name" name="from_name"/>  
-            <input type="text" className="formFromBox" placeholder="Your e-mail" name="user_email"/>
-            {/* <input type="text" className="formFromBox" placeholder="Subject"/> */}
-            <textarea type="text" className="formMessageBox" name="message"/>
+            <input type="text" className="formFromBox" placeholder="Your name" name="from_name" ref={nameResetValues}/>  
+            <input type="text" className="formFromBox" placeholder="Your e-mail" name="user_email" ref={emailResetValues}/>
+            <textarea type="text" className="formMessageBox" name="message" ref={textAreaResetValues}/>
             <button type="submit" className="formButton">SEND</button>
             
             <Link to='/'>
